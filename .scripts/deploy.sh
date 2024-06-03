29de3acf8a062899066d47e97009526f4c5b86dc
Kunto Bregananta Adi <bregananta@gmail.com>
2024-06-03 11:01:29 +0700

    dev deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "Deployment started ..."
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin production
+git pull origin development
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
