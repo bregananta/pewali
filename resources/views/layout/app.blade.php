12d107f0d2f637ee6ef0da47d7b4f968510d3437
Kunto Bregananta Adi <bregananta@gmail.com>
2024-06-03 11:02:59 +0700

    test deploy

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>{{ $title ?? config('app.name', 'CV Pewali') }}</title>
+    <title>{{ $title ?? config('app.name', 'CV Pewali') }} --</title>
     <meta name="description" content="AIMass Tailwind based SASS Template" />
 
     <!-- Favicon  -->
