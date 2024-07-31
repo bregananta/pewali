29af2f40a9f9307eeb25f5766aa80bcd70088668
Kunto Bregananta Adi <bregananta@gmail.com>
2024-07-31 10:47:30 +0700

    back to old folder

--- deploy.php
+++ deploy.php
@@ -40,7 +40,7 @@ host('Production_Server')
     ->set('remote_user', 'bregananta')
     ->set('port', 7822)
     ->set('branch', 'main')
-    ->set('deploy_path', '/var/www/pewali-new');
+    ->set('deploy_path', '/var/www/pewali');
 
 task('deploy:secrets', function () {
     file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
