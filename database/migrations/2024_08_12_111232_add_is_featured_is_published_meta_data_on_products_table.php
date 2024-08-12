6c82e1e3235f8ddb1c3992a3d7f118a5e4bde2ed
Kunto Bregananta Adi <bregananta@gmail.com>
2024-08-12 20:38:43 +0700

    update migration

--- database/migrations/2024_08_12_111232_add_is_featured_is_published_meta_data_on_products_table.php
+++ database/migrations/2024_08_12_111232_add_is_featured_is_published_meta_data_on_products_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('sku', 45)->unique()->after('product_category_id');
             $table->boolean('is_featured')->default(false)->after('description');
             $table->boolean('is_published')->default(false)->after('description');
             $table->text('meta')->nullable();
@@ -25,7 +24,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('sku');
             $table->dropColumn('is_published');
             $table->dropColumn('is_featured');
             $table->dropColumn('meta');
